09821cca72521d81248d8bd10cfa98e8a5feca6c
osimuka <hsojo91@gmail.com>
2017-11-18 00:34:24 +0000

    Update settings.py

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
-        'USER': 'uka.osim',
+        'USER': 'root',
         'PASSWORD': 'django',
         'HOST': '127.0.0.1',
         'PORT': '5432',
